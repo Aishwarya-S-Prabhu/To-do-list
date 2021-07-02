ba68fd5c59ac58b900c626c767119cdfa905cf0e
Aishwarya-S-Prabhu <ayshwaria.achu@gmail.com>
2021-07-02 21:19:03 +0530

    brief outline

---
+++ index.js
@@ -0,0 +1,13 @@
+let button = document.getElementById('button')
+let div = document.getElementById('display-data')
+let input = document.getElementById('input')
+
+button.addEventListener('click',() =>{
+    var list =document.createElement('p')
+    list.classList.add('paragraph')
+    list.innerText = input.value
+    div.appendChild(list)
+    input.value =""
+    list.addEventListener('click',() => list.style.textDecoration = "line-through")
+    list.addEventListener('dblclick',() =>div.removeChild(list  ))
+})
